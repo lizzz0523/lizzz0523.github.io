e260f98fd81cbfa246a38426f3e0fb9c489550ad
Liz Chow <lizzzled@gmail.com>
2014-01-13 17:35:37 +0800

    update style

--- js/index.js
+++ js/index.js
@@ -1,37 +1,17 @@
 (function($, window, undefined){
 
-var speed = 400;
+$('#projects')
 
-$('#projects').on('mouseenter', 'li', function(){
-    var $self = $(this),
+.find('li p')
+.css({
+    'display': 'table',
+    'height': '100%'
+})
 
-        $shadow = $self.find('.shadow'),
-        $title = $self.find('h3'),
-        $desc = $self.find('p');
-
-    $shadow.stop().animate({opacity : 0.5}, {
-        duration : speed,
-        step : function(now, tween){
-            $title.css({opacity : 1 - tween.pos});
-            $desc.css({opacity : tween.pos,  marginTop : 20 * (1 - tween.pos)});
-        }
-    });
+.wrapInner('<i />')
+.css({
+    'display': 'table-cell',
+    'vertical-align': 'middle'
 });
-
-$('#projects').on('mouseleave', 'li', function(){
-    var $self = $(this),
-
-        $shadow = $self.find('.shadow'),
-        $title = $self.find('h3'),
-        $desc = $self.find('p');
-
-    $shadow.stop().animate({opacity : 0}, {
-        duration : speed,
-        step : function(now, tween){
-            $title.css({opacity : tween.pos});
-            $desc.css({opacity : 1 - tween.pos,  marginTop : 20 * tween.pos});
-        }
-    });
-});
-
+    
 })(jQuery, this);
