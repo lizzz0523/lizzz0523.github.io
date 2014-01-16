fb708a935728bbbf30ed67c62cd7b98ceca9616c
lizzz0523 <lizzzled@gmail.com>
2014-01-16 23:47:49 +0800

    test liquid strip_newlines filter

--- js/blog.js
+++ js/blog.js
@@ -1,6 +1,6 @@
 (function($, window, undefined){
 
-$.getJSON('http://lizzz0523.github.io/data/blog-list.json', function(data){
+$.getJSON('http://lizzz0523.github.io/data/blog-list.json?' + Math.random(), function(data){
     console.log(data);
 });
     
