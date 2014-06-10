aab12e013d47287a3f9391c84dd7d0918b29c173
lizzz0523 <lizzzled@gmail.com>
2014-06-11 00:56:02 +0800

    debug

--- js/sea-config.js
+++ js/sea-config.js
@@ -1,8 +1,8 @@
 seajs.config({
     // 别名配置
     alias: {
-      'backbone': 'sea-modules/backbone/1.1.2/backbone',
-      'underscore': 'sea-modules/underscore/1.6.0/underscore',
+      'backbone': 'sea-modules/backbone/1.1.2/backbone-min',
+      'underscore': 'sea-modules/underscore/1.6.0/underscore-min',
       'jquery': 'sea-modules/jquery/1.8.3/jquery.min'
     },
 
