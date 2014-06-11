263920a9b359cab1afd3b080cbbac0a8f21bd156
Liz Chow <lizzzled@gmail.com>
2014-06-11 10:26:26 +0800

    debug

--- js/sea-config.js
+++ js/sea-config.js
@@ -1,13 +1,13 @@
 seajs.config({
-    // 别名配置
-    alias: {
-      'backbone': 'sea-modules/backbone/1.1.2/backbone-min',
-      'underscore': 'sea-modules/underscore/1.6.0/underscore-min',
-      'jquery': 'sea-modules/jquery/1.8.3/jquery.min'
-    },
+    // 预加载项
+    preload : [
+      'sea-modules/backbone/1.1.2/backbone-min',
+      'sea-modules/underscore/1.6.0/underscore-min',
+      'sea-modules/jquery/1.8.3/jquery.min'
+    ],
 
     // 路径配置
-    paths: {
+    paths : {
       'app': 'app-modules'
     },
 
