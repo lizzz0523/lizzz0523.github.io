99573f5976783be8bf558ede3d52d09a5a4e7468
Liz Chow <lizzzled@gmail.com>
2014-06-11 11:57:23 +0800

    debug

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ seajs.use(['jquery', 'app/model/Projects', 'app/view/ProjectList'], function($, 
         });
 
     $.getJSON('http://lizzz0523.github.io/data/projects.json?' + Math.random(), function(data) {
-        $.each(data, function(data) {
+        $.each(data, function(index, data) {
             projects.add(data, {silent : true});
         });
 
