c5fd80d5aa7c3a7fb542890db957e94b850bd501
Liz Chow <lizzzled@gmail.com>
2017-01-23 17:05:02 +0800

    update

--- demo/listview/index.js
+++ demo/listview/index.js
@@ -160,7 +160,7 @@
 	    },
 
 	    mounted: function mounted() {
-	        this.isIOS = !!navigator.userAgent.match(/ip(hone|ad|od)/i);
+	        this.isIOS = true; //!!navigator.userAgent.match(/ip(hone|ad|od)/i);
 
 	        if (this.isIOS) {
 	            this.scroller = new IScroll(this.$el.parentNode, {
