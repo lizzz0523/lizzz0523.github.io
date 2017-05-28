7b3b2bf6c3864f8c98bbcf2961dd056934f49d00
Liz Chow <lizzzled@gmail.com>
2017-05-28 16:31:02 +0800

    update

---
+++ demo/netbar/app.js
@@ -0,0 +1,96 @@
+let cacheKey = "netbar-app-1";
+let cacheList = [
+    "index.html",
+    "assets/css/common.css",
+    "assets/css/index.css",
+    "assets/img/index.png",
+    "assets/img/food.jpg",
+    "assets/img/banner.jpg",
+    "assets/img/netbar1.jpg",
+    "assets/img/netbar2.jpg",
+    "assets/img/netbar3.jpg",
+    "assets/img/netbar4.jpg",
+    "assets/img/netbar5.jpg",
+    "assets/img/netbar6.jpg",
+    "assets/img/netbar7.jpg",
+    "assets/img/netbar8.jpg",
+    "assets/img/netbar9.jpg",
+    "assets/img/netbar10.jpg"
+];
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches
+            .open(cacheKey)
+            .then(function (cache) {
+                cache.addAll(cacheList)
+            })
+    );
+});
+
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches
+        .keys()
+        .then(function (keys) {
+            return Promise.all(
+                keys.map(function (key) {
+                    if (key !== cacheKey) {
+                        return caches.delete(key);
+                    } else {
+                        return Promise.resolve();
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", function (event) {
+    var request = event.request;
+    var url = request.url;
+
+    var match = url.match(/^https:\/\/lizzz0523.github.io\/demo\/netbar\/(.*)$/);
+    if (!match) return;
+
+    var path = match[1];
+
+    if (cacheList.indexOf(path) > -1) {
+        event.respondWith(
+            respondWithCache(request);
+        );
+    }
+});
+
+function respondWithCache(request) {
+    return caches
+    .match(request)
+    .then(function (response) {
+        if (!response) {
+            throw Error("no response");
+        }
+
+        return response.clone();
+    })
+    .catch(function () {
+        var req = request.clone();
+
+        return fetch(req)
+        .then(function (res) {
+            if (!res || res.status != 200 || res.type != "basic") {
+                return res;
+            } else {
+                var response = res.clone();
+
+                return caches
+                .open(cacheKey)
+                .then(function (cache) {
+                    return cache.put(request, response);
+                })
+                .then(function () {
+                    return res;
+                });
+            }
+        });
+    });
+}
